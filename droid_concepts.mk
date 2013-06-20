bb7ec31c469d983ccb54021982a1aabd178a0080
davros <davros@droidconcepts.com>
2013-06-20 10:29:04 -0400

    yep

--- droid_concepts.mk
+++ droid_concepts.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/DEMENTED/config/common_gsm.mk)
-$(call inherit-product, vendor/DEMENTED/config/common_phone.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_gsm.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
-PRODUCT_NAME := demented_jfltetmo
+PRODUCT_NAME := droid_concepts_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 
